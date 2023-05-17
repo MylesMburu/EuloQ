ea9b220c1814d34b69d38e6cd361b9b1b5a1fea5
MylesMburu <ngicumyles@gmail.com>
2023-05-17 17:32:10 +0300

    ..

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Landing_Page } from './Landing_Page'
+
+export const App = () => {
+  return (
+   <>
+   <Landing_Page/>
+   </>
+  )
+}
---
+++ src/Landing_Page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Landing_Page = () => {
+  return (
+    <div>Landing_Page</div>
+  )
+}
